7995fa1b61e60c978513f09d775dcb525eb40ae3
xuehui <lxhzmy@126.com>
2019-07-24 16:39:14 +0800

    add word count demo

---
+++ src/main/java/com/lixuehui/flink/WordCount.java
@@ -0,0 +1,45 @@
+package com.lixuehui.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class WordCount {
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        String outputPath = "/path/out/file";
+
+        DataSet<String> text = env.readTextFile("/path/to/file");
+
+        DataSet<Tuple2<String, Integer>> counts =
+                // split up the lines in pairs (2-tuples) containing: (word,1)
+                text.flatMap(new Tokenizer())
+                        // group by the tuple field "0" and sum up tuple field "1"
+                        .groupBy(0)
+                        .sum(1);
+
+        counts.writeAsCsv(outputPath, "\n", " ");
+
+    }
+    // User-defined functions
+    public static class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<String, Integer>(token, 1));
+                }
+            }
+        }
+    }
+}
